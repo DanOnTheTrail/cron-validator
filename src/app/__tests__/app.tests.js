ac0f05f85608b6237f75eb7a3360ff166ca74203
Dan Cook <5843830+DanOnTheTrail@users.noreply.github.com>
2022-12-20 23:48:50 -0500

    wire up the app

---
+++ src/app/__tests__/app.tests.js
@@ -0,0 +1,53 @@
+const sut = require('../index');
+
+test('displays success message when input is valid', () => {
+  document.body.innerHTML =
+    '<button id="validate">Validate</button>'+
+    '<input type="text" id="cron" />' +
+    '<p id="message-box"></p>';
+  document.getElementById('cron').setAttribute("value", "magic");
+  const mockValidator = function () {
+    return 'VALID';
+  }
+  sut.init(mockValidator);
+
+
+  document.getElementById('validate').click();
+  const result = document.getElementById('message-box').textContent;
+
+  expect(result).toBe('Valid cron expression!');
+});
+
+test('displays error message when input is invalid', () => {
+  document.body.innerHTML =
+    '<button id="validate">Validate</button>'+
+    '<input type="text" id="cron" />' +
+    '<p id="message-box"></p>';
+  document.getElementById('cron').setAttribute("value", "magic");
+  const mockValidator = function () {
+    return 'INVALID_LENGTH';
+  }
+  sut.init(mockValidator);
+
+  document.getElementById('validate').click();
+  const result = document.getElementById('message-box').textContent;
+
+  expect(result).toBe('Invalid cron expression!');
+});
+
+test('displays unknown error message when status is unknown', () => {
+  document.body.innerHTML =
+    '<button id="validate">Validate</button>'+
+    '<input type="text" id="cron" />' +
+    '<p id="message-box"></p>';
+  document.getElementById('cron').setAttribute("value", "magic");
+  const mockValidator = function () {
+    return '';
+  }
+  sut.init(mockValidator);
+
+  document.getElementById('validate').click();
+  const result = document.getElementById('message-box').textContent;
+
+  expect(result).toBe('Unrecognized error');
+});
